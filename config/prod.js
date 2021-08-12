dd3cd7085312f245243a64a29aec52bbffd35036
tankyhuynh <tankyhuynh>
2021-08-12 17:19:15 +0700

    NodeJS with Upload

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
+    // mongoURI:process.env.MONGO_URI
+    mongoURI:'mongodb+srv://tanky:OhqPl5JoYr3CD5kL@cluster0.w9x67.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
