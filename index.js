21e75727a290f9b3943ca8a5af20db04e1759470
tankyhuynh <tankyhuynh>
2021-08-12 17:16:59 +0700

    NodeJS with Upload

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ app.use('/api/projects', require('./routes/projects'));
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 
-  // Set static folder
-  app.use(express.static("client/build"));
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   // index.html for all page routes
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 3001
 
