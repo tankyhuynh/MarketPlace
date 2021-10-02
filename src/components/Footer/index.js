b19a981a3868a3f748b3c47d9996f88ec2b7057f
tankyhuynh <tankyhuynh>
2021-10-02 17:06:58 +0700

    Show message in auth - Footer project show

---
+++ src/components/Footer/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="flex flex-col w-2/3 max-w-lg gap-2 mx-auto italic text-left ">
+                <div className="font-bold">
+                    Thông tin vui lòng gửi về đơn vị đầu mối của Sở Khoa học và Công nghệ:
+                </div>
+                <div>
+                    Trung tâm Thông tin Khoa học và Công nghệ Cần Thơ
+                </div>
+                <div>
+                    Địa chỉ: 118/3 Trần Phú, P. Cái Khế, Q. Ninh Kiều, TP. Cần Thơ
+                </div>
+                <div>
+                    ĐT: 0292.3824031- Fax: 0292.3812352 (Gặp Chà My)
+                </div>
+                <div>
+                    Email: trungtamttkhcn@cantho.gov.vn hoặc
+                </div>
+                <div>
+                    phongttkhcn.casti@gmail.com
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
